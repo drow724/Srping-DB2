c21d692420bebce1e9a7d359ca9cb90a225f1650
Macintosh <drow724@naver.com>
2022-08-02 09:51:47 +0900

    프로젝트 생성

---
+++ springtx/src/main/java/hello/springtx/SpringtsApplication.java
@@ -0,0 +1,13 @@
+package hello.springtx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtsApplication.class, args);
+	}
+
+}
